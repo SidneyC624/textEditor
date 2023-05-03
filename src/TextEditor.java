c52a196f94cf1e75848ca06b4e117e66526f1ac6
SidneyC624 <sidney.0503c@gmail.com>
2023-05-03 17:39:06 +0800

    change color and font

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,8 +1,14 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
@@ -18,6 +24,9 @@ public class TextEditor extends JFrame implements ActionListener{
 	JScrollPane scrollPane;
 	JSpinner fontSizeSpinner;
 	JLabel fontLabel;
+	JButton fontColorButton;
+	JComboBox fontBox;
+	String[] fonts;
 	
 	TextEditor() {
 		
@@ -52,8 +61,20 @@ public class TextEditor extends JFrame implements ActionListener{
 			
 		});
 		
+		fontColorButton = new JButton("Color");
+		fontColorButton.setFocusable(false);
+		fontColorButton.addActionListener(this);
+		
+		fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+		
+		fontBox = new JComboBox(fonts);
+		fontBox.addActionListener(this);
+		fontBox.setSelectedItem("Arial");
+		
 		this.add(fontLabel);
 		this.add(fontSizeSpinner);
+		this.add(fontColorButton);
+		this.add(fontBox);
 		this.add(scrollPane);
 		this.setVisible(true);
 		
@@ -62,6 +83,17 @@ public class TextEditor extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
+		if(e.getSource() == fontColorButton) {
+			
+			JColorChooser colorChooser = new JColorChooser();
+			
+			Color color = colorChooser.showDialog(null, "Choose a color", Color.black);
+			textArea.setForeground(color);
+		}
+		
+		if(e.getSource() == fontBox) {
+			textArea.setFont(new Font((String) fontBox.getSelectedItem(), Font.PLAIN, textArea.getFont().getSize()));
+		}
 		
 	}
 
