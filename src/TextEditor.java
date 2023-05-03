cd67c1b0f84fbff238dc2043c8be16beedb5a0ef
SidneyC624 <sidney.0503c@gmail.com>
2023-05-03 17:09:18 +0800

    can change font size with spinner

---
+++ src/TextEditor.java
@@ -0,0 +1,68 @@
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class TextEditor extends JFrame implements ActionListener{
+
+	JTextArea textArea;
+	JScrollPane scrollPane;
+	JSpinner fontSizeSpinner;
+	JLabel fontLabel;
+	
+	TextEditor() {
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("My Text Editor");
+		this.setSize(500,500);
+		this.setLayout(new FlowLayout());
+		this.setLocationRelativeTo(null);
+		
+		textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		textArea.setFont(new Font("Arial", Font.PLAIN, 20));
+		
+		scrollPane = new JScrollPane(textArea);
+		scrollPane.setPreferredSize(new Dimension(450,450));
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		fontLabel = new JLabel("Font: ");
+		
+		fontSizeSpinner = new JSpinner();
+		fontSizeSpinner.setPreferredSize(new Dimension(50, 25));
+		fontSizeSpinner.setValue(20);
+		fontSizeSpinner.addChangeListener(new ChangeListener(){
+
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				
+				textArea.setFont(new Font(textArea.getFont().getFamily(), Font.PLAIN, (int) fontSizeSpinner.getValue()));
+				
+			}
+			
+		});
+		
+		this.add(fontLabel);
+		this.add(fontSizeSpinner);
+		this.add(scrollPane);
+		this.setVisible(true);
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
+
+}
