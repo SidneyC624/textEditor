1d9204013267168e91ac7383550de7a5281eed4d
SidneyC624 <sidney.0503c@gmail.com>
2023-05-03 18:44:25 +0800

    create menu bar for open and save

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -5,18 +5,27 @@ import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class TextEditor extends JFrame implements ActionListener{
 
@@ -27,6 +36,12 @@ public class TextEditor extends JFrame implements ActionListener{
 	JButton fontColorButton;
 	JComboBox fontBox;
 	String[] fonts;
+	
+	JMenuBar menuBar;
+	JMenu fileMenu;
+	JMenuItem openItem;
+	JMenuItem saveItem;
+	JMenuItem exitItem;
 	
 	TextEditor() {
 		
@@ -71,6 +86,26 @@ public class TextEditor extends JFrame implements ActionListener{
 		fontBox.addActionListener(this);
 		fontBox.setSelectedItem("Arial");
 		
+		//-----menu bar -----
+		
+		menuBar = new JMenuBar();
+		fileMenu = new JMenu("File");
+		openItem = new JMenuItem("Open");
+		saveItem = new JMenuItem("Save");
+		exitItem = new JMenuItem("Exit");
+		
+		openItem.addActionListener(this);
+		saveItem.addActionListener(this);
+		exitItem.addActionListener(this);
+		
+		fileMenu.add(openItem);
+		fileMenu.add(saveItem);
+		fileMenu.add(exitItem);
+		menuBar.add(fileMenu);
+				
+		//-----/menu bar-----
+		
+		this.setJMenuBar(menuBar);
 		this.add(fontLabel);
 		this.add(fontSizeSpinner);
 		this.add(fontColorButton);
@@ -95,6 +130,68 @@ public class TextEditor extends JFrame implements ActionListener{
 			textArea.setFont(new Font((String) fontBox.getSelectedItem(), Font.PLAIN, textArea.getFont().getSize()));
 		}
 		
+		if(e.getSource() == openItem) {
+			
+			JFileChooser fileChooser = new JFileChooser();
+			fileChooser.setCurrentDirectory(new File("."));
+			
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files", "txt");
+			fileChooser.setFileFilter(filter);
+			
+			int response = fileChooser.showOpenDialog(null);
+			
+			if(response == JFileChooser.APPROVE_OPTION) {
+				File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+				Scanner fileIn = null;
+				
+				try {
+					fileIn = new Scanner(file);
+					if(file.isFile()) {
+						while(fileIn.hasNextLine()) {
+							String line = fileIn.nextLine() + "\n";
+							textArea.append(line);
+						}
+					}
+				} catch (FileNotFoundException e1) {
+					
+					e1.printStackTrace();
+				}
+				finally {
+					fileIn.close();
+				}
+				
+			}
+			
+		}
+		
+		if(e.getSource() == saveItem) {
+			JFileChooser fileChooser = new JFileChooser();
+			fileChooser.setCurrentDirectory(new File("."));
+			
+			int response = fileChooser.showSaveDialog(null);
+			
+			if(response == JFileChooser.APPROVE_OPTION) {
+				File file;
+				PrintWriter fileOut = null;
+				
+				file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+				try {
+					fileOut = new PrintWriter(file);
+					fileOut.println(textArea.getText());
+				} catch (FileNotFoundException e1) {
+					
+					e1.printStackTrace();
+				}
+				finally {
+					fileOut.close();
+				}
+			}
+			
+		}
+		
+		if(e.getSource() == exitItem) {
+			System.exit(0);
+		}
 	}
 
 }
